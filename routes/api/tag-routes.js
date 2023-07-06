b22ddfb9eeed496743ac58bbb8ae536d1dff9395
Anthony-D99 <anthonydisaf@yahoo.com>
2023-07-06 11:37:39 -0400

    Reformated readme.md file to complete acceptance criteria and removed all comments from all files.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,11 +1,8 @@
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
-// The `/api/tags` endpoint
 
 router.get('/', async (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
   try {
     const tagData =  await Tag.findAll({
       include: [{ model: Product }]
@@ -17,8 +14,6 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
   try {
     const tagData =  await Tag.findByPk(req.params.id, {
       include: [{ model: Product }]
@@ -36,7 +31,6 @@ router.get('/:id', async (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  // create a new tag
   try {
     const tagData = Tag.create(req.body);
     res.status(200).json(tagData);
@@ -46,7 +40,6 @@ router.post('/', (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
   Tag.update(req.body.name, {
     where: {
       id: req.params.id,
@@ -60,7 +53,6 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
   try {
     const tagData =  Tag.destroy({
       where: {
